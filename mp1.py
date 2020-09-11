9c6e0bd1f66dbc431947db907928c6538f003d4d
ecn3 <43419606+ecn3@users.noreply.github.com>
2020-09-11 14:45:52 -0500

    changed python to 3.8.5

--- mp1.py
+++ mp1.py
@@ -1,7 +1,21 @@
-# Output
+""" 
+Christian Nelson
+9/11/2020
+CPSC-57100-002, Fall 2020
+Machine Problem 1: Sliding Puzzle
+"""
 
+# Imports
+import numpy as np
+import queue
+#from multiprocessing import Queue
+
+# Output Heading
 print('Artificial Intelligence')
 print('MP1: A* for Sliding Puzzle')
 print('SEMESTER: Fall 2020')
 print('NAME: Christian Nelson')
 print("")
+
+# load random start state onto frontier priority queue
+frontier = queue.PriorityQueue()
